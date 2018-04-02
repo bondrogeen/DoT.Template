c112468983f7cfd6408e1d278f946e90ba1a590a
Roman <bondrogeen@gmail.com>
2018-04-02 17:42:03 +0300

    ...

--- files/Template.lua
+++ files/Template.lua
@@ -1,6 +1,16 @@
 return function (t)
-if adc.force_init_mode(adc.INIT_VDD33)
-then
-  print(adc.read())
-end
+ local r
+ if adc then
+  if adc.force_init_mode(t.adc and adc.INIT_VDD33 or adc.INIT_ADC)then
+   print("ESP needs to be restarted for the change to take effect")
+   node.restart()
+   return
+  else
+   r= t.adc and "System voltage (mV): "..adc.readvdd33(0) or "adc : "..adc.read(0)
+   print(r)
+  end
+ else
+ print("The adc module is not installed")
+ end
+ return r
 end
