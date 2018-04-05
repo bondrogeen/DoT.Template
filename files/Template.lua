64ff314d3cda8d0c783b07e58d98d2c711c78620
Roman <bondrogeen@gmail.com>
2018-04-05 23:56:21 +0300

    fix

--- files/Template.lua
+++ files/Template.lua
@@ -1,12 +1,12 @@
 return function (t)
  local r
- if adc then
-  if adc.force_init_mode(t.adc and adc.INIT_VDD33 or adc.INIT_ADC)then
+ if run then
+  if adc.force_init_mode(t.adc=='vdd' and adc.INIT_VDD33 or adc.INIT_ADC)then
    print("ESP needs to be restarted for the change to take effect")
    node.restart()
    return
   else
-   r= t.adc and "System voltage (mV): "..adc.readvdd33(0) or "adc : "..adc.read(0)
+   r= t.adc=='vdd' and "System voltage (mV): "..adc.readvdd33(0) or "adc : "..adc.read(0)
    print(r)
   end
  else
